c7930b8153d26b4a6afae295fe80d692b17c7ca5
cj-planetOdoo <89021095+cj-planetOdoo@users.noreply.github.com>
2022-04-15 14:34:52 +0530

    Delete l10n_in_einvoice directory

--- l10n_in_einvoice/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import res_partner
-from . import account_move
-from . import einvoice_service
-from . import einvoice_transaction
